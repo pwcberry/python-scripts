03e79e318baee8c7721d1a3dded95e620b48b73d
Peter Berry <pwcberry@gmail.com>
2025-08-01 15:32:28 +1000

    Refactor credentials code

--- google-cloud-platform/bulk-up-my-calendar/main.py
+++ google-cloud-platform/bulk-up-my-calendar/main.py
@@ -1,12 +1,8 @@
 import datetime
-import os.path
 import json
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import credentials
 
 SCOPES = [
     "https://www.googleapis.com/auth/calendar.events",
@@ -14,20 +10,7 @@ SCOPES = [
 ]
 
 def main():
-    creds = None
-
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    creds = credentials.retrieve_gcp_credentials()
 
     football_matches = []
     with open("essendon-afl-2025.json", "r") as file:
